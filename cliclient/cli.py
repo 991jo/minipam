563e6ffbb8b9d1f789d09e1a534f7e09e622df6f
Johannes Erwerle <johannes.erwerle@googlemail.com>
2018-06-18 13:26:36 +0200

    new features for the cliclient
    - server can now be given in a file in ~/.minipamcli.json
      the format of this file is
    
        }
    	"server" : "http://localhost:8000/"
        }
    
    - help messages added to all commands
    - changed printing for get_net, networks which are only here for the
    query and are not in the database are shown in brackets and have a not
    attached to them.

--- cliclient/cli.py
+++ cliclient/cli.py
@@ -1,20 +1,39 @@
 import argparse
+import os
+import json
+import sys
+
 from xmlrpc.client import ServerProxy
 from pprint import PrettyPrinter
 
 parser = argparse.ArgumentParser(description="a CLI client for minipam")
-parser.add_argument("server", nargs=1, type=str, metavar="server")
+parser.add_argument("--server", nargs=1, type=str, metavar="SERVER")
 group = parser.add_mutually_exclusive_group()
-group.add_argument("--add-net","-an", nargs=1, type=str, metavar="NET")
-group.add_argument("--delete-net","-dn", nargs=1, type=str, metavar="NET")
-group.add_argument("--get-net","-gn", nargs=1, type=str, metavar="NET")
-group.add_argument("--claim-net","-cn", nargs=2, type=str, metavar=("NET", "PREFIXLEN"))
-group.add_argument("--add-tag","-at", nargs=3, type=str, metavar=("NET", "TAGNAME", "TAGVALUE"))
-group.add_argument("--delete-tag","-dt", nargs=2, type=str, metavar=("NET", "TAGNAME"))
-group.add_argument("--modify-tag","-mt", nargs=3, type=str, metavar=("NET", "TAGNAME", "TAGVALUE"))
-group.add_argument("--get-tag","-gt", nargs=2, type=str, metavar=("NET", "TAGNAME"))
-group.add_argument("--get-tags","-gts", nargs=1, type=str, metavar="NET")
+group.add_argument("--add-net","-an", nargs=1, type=str, metavar="NET", help="add the network NET")
+group.add_argument("--delete-net","-dn", nargs=1, type=str, metavar="NET", help="delete the network NET")
+group.add_argument("--get-net","-gn", nargs=1, type=str, metavar="NET", help="get a network and it's children")
+group.add_argument("--claim-net","-cn", nargs=2, type=str, metavar=("NET", "NETMASK"), help="claim a new network with a subnet mask of NETMASK in the network NET")
+group.add_argument("--add-tag","-at", nargs=3, type=str, metavar=("NET", "TAGNAME", "TAGVALUE"), help="add a tag to the network NET with the name TAGNAME and value TAGVALUE")
+group.add_argument("--delete-tag","-dt", nargs=2, type=str, metavar=("NET", "TAGNAME"), help="delete the tag TAGNAME from the network NET")
+group.add_argument("--modify-tag","-mt", nargs=3, type=str, metavar=("NET", "TAGNAME", "TAGVALUE"), help="modify the value of the tag TAGNAME to TAGVALUE for the network NET")
+group.add_argument("--get-tag","-gt", nargs=2, type=str, metavar=("NET", "TAGNAME"), help="get the value of the tag TAGNAME for network NETWORK")
+group.add_argument("--get-tags","-gts", nargs=1, type=str, metavar="NET", help="get all tags from the network NET")
 args = parser.parse_args()
+
+if args.server is None:
+    # check for ~/.minipamcli.json and take the server param from there
+    config_path = os.path.expanduser("~/.minipamcli.json")
+    if os.path.isfile(config_path):
+        with open(config_path, "r") as f:
+            config = json.loads(f.read())
+            if "server" in config:
+                args.server = [config["server"]]
+            else:
+                print("key 'server' not found in %s" % config_path)
+                sys.exit(1)
+    else:
+        print("no --server argument and no %s found." % config_path)
+        sys.exit(1)
 
 with ServerProxy(args.server[0]) as server:
     pp = PrettyPrinter()
@@ -23,7 +42,10 @@ with ServerProxy(args.server[0]) as server:
         result = server.get_net(args.get_net[0])
 
         def recursive_print(net, indent=0):
-            print(" "*indent, net["cidr"])
+            if net["network_in_database"]:
+                print(" "*indent, net["cidr"])
+            else:
+                print(" "*indent, "(%s) - not in database" % net["cidr"])
             for child in net["children"]:
                 recursive_print(child, indent=indent+2)
 
